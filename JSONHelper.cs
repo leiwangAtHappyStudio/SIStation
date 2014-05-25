e4b279d0e276367a15eec17ca340442d0fc539e4
leiwangAtHappyStudio <hbrock88@hotmail.com>
2014-05-25 12:41:12 +0800

    initialization
    Initialization

---
+++ JSONHelper.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SIStation  
+{  
+    /// <summary>  
+    /// Json序列化和反序列化辅助类   
+    /// </summary>  
+    public class JSONHelper  
+    {  
+        /// <summary>  
+        /// Json序列化   
+        /// </summary>    
+        /// <param name="obj">json对象实例</param>  
+        /// <returns>json字符串</returns>  
+        public static string JsonSerializer(JObject obj)
+        {  
+            string jsonString;  
+            try  
+            {
+                jsonString = obj.ToString();
+            }  
+            catch  
+            {  
+                jsonString = string.Empty;  
+            }  
+            return jsonString;  
+        }  
+  
+  
+        /// <summary>  
+        /// Json反序列化  
+        /// </summary>  
+        /// <param name="jsonString">json字符串</param>  
+        /// <returns>对象实例</returns>  
+        public static JObject JsonDeserialize(string jsonString)  
+        {
+            JObject obj = null;
+            try
+            {
+                obj = JObject.Parse(jsonString);
+            }
+            catch
+            {
+            }
+            return obj;
+        }  
+  
+  
+        /// <summary>  
+        /// 将 DataTable 序列化成 json 字符串  
+        /// </summary>  
+        /// <param name="dt">DataTable对象</param>  
+        /// <returns>json 字符串</returns>  
+        public static List<JObject> DataTableToJson(DataTable dt)  
+        {  
+            if (dt == null || dt.Rows.Count == 0)  
+            {  
+                return null;  
+            }
+
+            List<JObject> tableData = new List<JObject>();
+            foreach (DataRow dr in dt.Rows)  
+            {  
+                JObject rowObj = new JObject();
+                foreach (DataColumn dc in dt.Columns)  
+                {  
+                    rowObj.Add(dc.ColumnName, JToken.FromObject(dr[dc]));
+                }
+                tableData.Add(rowObj);
+            }
+            return tableData;  
+        }
+
+        /// <summary>  
+        /// 将 json对象输出到Excel 
+        /// </summary>  
+        /// <param name="dt">json对象</param>  
+        /// <returns>Excel</returns>  
+        
+    }  
+} 
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using Excel = Microsoft.Office.Interop.Excel;
+using System.IO;
+using System.Reflection;
+
+namespace SIStation
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Excel.Application m_Excel = new Excel.Application();
+            m_Excel.SheetsInNewWorkbook = 1;
+            Excel._Workbook m_Book = (Excel._Workbook)(m_Excel.Workbooks.Add(Missing.Value));//添加新工作簿
+            //Excel._Worksheet m_Sheet = (Excel._Worksheet)(m_Excel.Worksheets.Add(Missing.Value));
+            //这里先这么写。。= =！
+            Excel._Worksheet m_Sheet = (Excel._Worksheet)(m_Excel.Worksheets.Add(Missing.Value, 1, 1, 1));
+            m_Sheet.Name = "工资报表";
+            DateTime date = DateTime.Now;
+            int month = date.Month;
+
+            m_Book.SaveAs("MyExcel", Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Excel.XlSaveAsAccessMode.xlNoChange, Missing.Value, Missing.Value, Missing.Value, Missing.Value, Missing.Value);
+            m_Book.Close(false, Missing.Value, Missing.Value);
+            m_Excel.Quit();
+        }
+    }
+}
